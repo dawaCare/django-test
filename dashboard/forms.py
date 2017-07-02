3458b8787e1cccdf0892cf5812bdec93c56ca590
Jesse Naldo <naldojesse@jnaldoweb.com>
2017-07-02 17:37:18 -0400

    commented out unneccessary code

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -7,24 +7,24 @@ from django.shortcuts import redirect
 
 
 
-
-class Patient_form(forms.Form):
-    Patient_name = forms.CharField(label="Name", max_length=30)
-    condition = forms.CharField(widget=forms.TextInput, max_length=30)
-    date_input = models.DateTimeField(default=datetime.now, blank=True)
-
-    models = Patient
-    fields = ('Patient_name','condition', 'date_input')
-
-    def save(self, commit=True):
-        Patient_data = super(Patient_form, self).save(commit=False)
-        Patient_data.Patient_name = self.cleaned_data['Patient_name']
-        Patient_data.condition = self.cleaned_data['condition']
-        if commit:
-            Patient_data.save()
-            return redirect('dashboard')
-        else:
-            return redirect('form')
+#
+# class Patient_form(forms.Form):
+#     Patient_name = forms.CharField(label="Name", max_length=30)
+#     condition = forms.CharField(widget=forms.TextInput, max_length=30)
+#     date_input = models.DateTimeField(default=datetime.now, blank=True)
+#
+#     models = Patient
+#     fields = ('Patient_name','condition', 'date_input')
+#
+#     def save(self, commit=True):
+#         Patient_data = super(Patient_form, self).save(commit=False)
+#         Patient_data.Patient_name = self.cleaned_data['Patient_name']
+#         Patient_data.condition = self.cleaned_data['condition']
+#         if commit:
+#             Patient_data.save()
+#             return redirect('dashboard')
+#         else:
+#             return redirect('form')
 
 
 # class RegisterForm(UserCreationForm):
