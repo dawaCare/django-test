686fc7fdf99f5908ecaacc0a1a2f708513bd0ec3
Jesse Naldo <naldojesse@jnaldoweb.com>
2017-07-02 19:34:46 -0400

    started importing django-import-export app

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -3,6 +3,7 @@ from dashboard.models import Outpatient
 # Register your models here.
 
 class OutpatientAdmin(admin.ModelAdmin):
+    # list_display = ('Patient_name', 'age', 'location', 'condition', 'medication', 'initial_visit', 'followup_appt', 'reminder_freq')
     list_display = ('visit_date', 'first_name', 'last_name', 'age', 'gender')
 
 admin.site.register(Outpatient, OutpatientAdmin)
