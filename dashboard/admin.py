d8fc80a5f8f9e0d7560f5475710060326235ed68
Jesse Naldo <naldojesse@jnaldoweb.com>
2017-07-02 21:17:55 -0400

    added ImportExportMixin to outpatient

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -10,7 +10,7 @@ class OutpatientResource(ModelResource):
     class Meta:
         model = Outpatient
 
-class OutpatientAdmin(admin.ModelAdmin):
+class OutpatientAdmin(ImportExportMixin, admin.ModelAdmin):
     resource_class = OutpatientResource
     # list_display = ('Patient_name', 'age', 'location', 'condition', 'medication', 'initial_visit', 'followup_appt', 'reminder_freq')
     # list_display = ('visit_date', 'first_name', 'last_name', 'age', 'gender')
